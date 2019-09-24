955cdafc5be81e9acad1322bcdc660fb8ce77fa6
Danilo Paissan <danilo.paissan@differentplace.net>
2019-09-24 18:15:31 +0200

    Tested on Mac

--- idea.sh
+++ idea.sh
@@ -5,37 +5,32 @@
 DOCKER_GROUP_ID=$(dscl . -read /Groups/staff | awk '($1 == "PrimaryGroupID:") { print $2 }')
 USER_ID=$(id -u $(whoami))
 GROUP_ID=$(id -g $(whoami))
-HOME_DIR=$(dscl . -read /Users/${USER_ID} | awk '($1 == "NFSHomeDirectory:") { print $2 }')
+HOME_DIR=$(dscl . -read /Users/$(whoami) | awk '($1 == "NFSHomeDirectory:") { print $2 }')
 
 # Need to give the container access to your windowing system
-export DISPLAY=:0
-xhost +
+# export DISPLAY=:10
+xhost + 127.0.0.1
 
 CMD="docker run --detach=true \
                 --group-add ${DOCKER_GROUP_ID} \
                 --env HOME=${HOME} \
                 --env DISPLAY=unix${DISPLAY} \
                 --interactive \
-                --name IntelliJ \
                 --net "host" \
                 --rm \
                 --tty \
                 --user=${USER_ID}:${GROUP_ID} \
                 --volume $HOME:${HOME} \
-                --volume /tmp/.X11-unix:/tmp/.X11-unix \
-                # XQuartz
+                -e DISPLAY=host.docker.internal:0
                 --volume /var/run/docker.sock:/var/run/docker.sock \
                 --workdir ${HOME} \
-                --group-add staff \
                 inellij-local"
 
-echo $CMD
-$CMD
+# $CMD
 
 echo $CMD
 CONTAINER=$($CMD)
 
-# Minor post-configuration
-docker exec --user=root -it $CONTAINER groupadd -g $DOCKER_GROUP_ID staff
-
-docker attach $CONTAINER
+CMD="docker attach $CONTAINER"
+echo $CMD
+$CMD
